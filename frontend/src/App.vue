d31215e7b3428b82f06ec5cdcf5a8deaebd85bbf
BarryDong <286084845@qq.com>
2025-10-30 23:27:00 -0700

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/posts">Posts</router-link>
+      <router-link to="/events">Events</router-link>
+      <router-link to="/login">Login</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+nav {
+  display: flex;
+  gap: 10px;
+}
+</style>
