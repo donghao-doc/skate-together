a3f5ddb30845f8c1cb6adcb47b3394663650b3d4
BarryDong <286084845@qq.com>
2025-10-30 23:36:05 -0700

    Create Home.vue

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>一起滑板</h1>
+    <p>欢迎来到滑板社区！</p>
+    <router-link to="/posts">查看帖子</router-link>
+    |
+    <router-link to="/events">查看活动</router-link>
+  </div>
+</template>
